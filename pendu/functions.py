cbaf59ea303dc882ac83ad7cf4dcd7094b432140
jjauzion <jeremy.jauzion@gmail.com>
2018-07-09 17:21:17 +0200

    jeu du pendu finnnni

--- pendu/functions.py
+++ pendu/functions.py
@@ -21,3 +21,24 @@ def update_hidden_letter(hidden_letter, letter):
         hidden_letter = hidden_letter.replace(letter[i], '')
         i += 1
     return hidden_letter
+
+import pickle
+
+def save_score(score, score_file):
+    name = input("Saissez votre pseudo pour sauvegarder votre score\n")
+    try:
+        with open(score_file, "rb") as fd:
+            depickler = pickle.Unpickler(fd)
+            score_dic = depickler.load()
+            if name not in score_dic.keys():
+                score_dic[name] = 0
+            else:
+                score_dic[name] += score
+            #score += 0 if score_dic.get(name) == None else score_dic.get(name)
+    except FileNotFoundError:
+        score_dic = {name: score}
+    with open(score_file, "wb") as fd:
+        my_pickle = pickle.Pickler(fd)
+        my_pickle.dump(score_dic)
+        print("Votre score a été ajouté!")
+        print("Pseudo : {} ; score : {}".format(name, score_dic[name]))
--- pendu/pendu.py
+++ pendu/pendu.py
@@ -1,26 +1,28 @@
 #!/usr/local/bin/python3.6
 # -*-coding:Utf-8 -*
 
-from random import randrange
+from random import choice
 import param
 from functions import *
 
-word = param.word_list[randrange(len(param.word_list))]
+word = choice(param.word_list)
 hidden_letter = "abcdefghifklmnopqrstuvwxyz"
 visible_word = gen_visible_word(hidden_letter, word)
 
 count = 8;
-while (count > 0 & visible_word.count("*")):
+while ((count > 0) & (visible_word.count("*") > 0)):
     print("\n------------------------------")
     print("Le mot du pendu :", visible_word)
     print("Il te reste {} chances".format(count))
     letter = get_letter()
     if len(letter) > 1:
+        score = count
         count = 0
     hidden_letter = update_hidden_letter(hidden_letter, letter)
     visible_word = gen_visible_word(hidden_letter, word)
     count -= 1
 if visible_word.count("*") <= 0:
     print("\n *** Bien joué! ***")
+    save_score(score, param.score_file)
 else:
     print("\n Perdu! Le mot était : ", word)
